ac9c89ed64d5e686448b1cbf1588263f6662cce0
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2024-11-19 11:36:37 +0100

    update the opt-ipt plotting

--- scripts/plot-opt-ipt.py
+++ scripts/plot-opt-ipt.py
@@ -23,7 +23,6 @@ def plot(file, hostname, jobid):
         return
 
     fig, axes = plt.subplots(nrows=NUM_ROWS, ncols=NUM_COLS, sharey="row", sharex="row")
-    fig.subplots_adjust(bottom=0.2, top=0.94, right=0.99, wspace=0.1, hspace=0.5)
 
     data = data.loc[(data["iteration"] >= utils.WARMUP) &
                     (data["phase"] == "selection")]
@@ -43,7 +42,7 @@ def plot(file, hostname, jobid):
     # labels on the bottom and left subplots
     for i in range(NUM_ROWS):
         axes = plt.subplot(NUM_ROWS, NUM_COLS, i * NUM_COLS + 1)
-        axes.set_ylabel("time (s)")
+        axes.set_ylabel("Speedup")
     for j in range(NUM_COLS):
         axes = plt.subplot(NUM_ROWS, NUM_COLS, (NUM_ROWS - 1) * NUM_COLS + j + 1)
         axes.set_xticks([4, 8, 12, 16])
@@ -53,16 +52,10 @@ def plot(file, hostname, jobid):
     # convert pd.Axes to list
     axes = fig.axes
 
-    colors = {}
-    i = 0
-    for alg, subgroup in data.groupby("algorithm"):
-        # generate a color for the algorithm
-        color = plt.cm.tab20(i)
-        colors[alg] = color
-        i += 1
+    peak = None
 
     # plot the speed-up
-    for (q, k), group in data.groupby(["query_count", "k"]):
+    for (q, k), group in data.groupby(["query_count", "k"], sort="true"):
         group = group.filter(items=["algorithm", "items_per_thread", "time"])
 
         q_index = data["query_count"].unique().tolist().index(q)
@@ -76,13 +69,27 @@ def plot(file, hostname, jobid):
         assert index < NUM_ROWS * NUM_COLS
 
         ax = axes[index]
-        ax.set_title(f"k={k} q={q}")
+        log_k = int(np.round(np.log2(k)))
+        log_q = int(np.round(np.log2(q)))
+        ax.set_title(f"k=$2^{{{log_k}}}$ q=$2^{{{log_q}}}$")
 
-        group = group.loc[group["items_per_thread"] >= 4]
+        mean_time = group.groupby(["algorithm", "items_per_thread"])['time'].mean().to_numpy()
+        base_time = group.loc[(group['items_per_thread'] == 1) & (group['algorithm'] == 'bits'), 'time'].mean()
+
+        if peak is None:
+            peak = np.max(base_time / mean_time) * 1.2
+        elif k == np.min(data["k"].unique()):
+            peak = min(np.max(base_time / mean_time) * 1.2, peak)
 
         # plot values for given k
+        algs = {}
         for alg, subgroup in group.groupby("algorithm"):
+            if alg not in algs:
+                algs[alg] = len(algs)
+
             mean_time = subgroup.groupby('items_per_thread')['time'].mean().to_numpy()
+
+            speedup = base_time / mean_time
 
             min_time_index = np.argmin(mean_time)
             min_time = subgroup["items_per_thread"].unique().astype(int)[min_time_index]
@@ -90,25 +97,26 @@ def plot(file, hostname, jobid):
             # plot speed-up
             ax.errorbar(
                 x=subgroup["items_per_thread"].unique().astype(int),
-                y=mean_time,
+                y=speedup,
                 linewidth=1,
                 capsize=5,
-                marker='.',
+                markersize=2,
+                marker=utils.SHAPES[algs[alg]],
                 label=alg,
-                color=colors[alg])
+                color=utils.COLORS[algs[alg]])
 
             # get color from the errorbar
-            color = ax.get_lines()[-1].get_color()
+            color = utils.COLORS[algs[alg]]
 
             # draw vertical line at the maximum speed-up
             ax.axvline(x=min_time, color=color, linestyle='--', linewidth=1)
 
             # draw horizontal line at the maximum speed-up
-            ax.axhline(y=mean_time[min_time_index], color=color, linestyle='--', linewidth=1)
+            ax.axhline(y=speedup[min_time_index], color=color, linestyle='--', linewidth=1)
 
             ax.set_xticks([4, 8, 12, 16])
-            ax.set_ylim(bottom=0)
-            ax.grid(alpha=0.4, linestyle="--")
+            ax.set_ylim(bottom=1., top=peak)
+            ax.grid(alpha=0.4, linestyle=":", which="both")
 
     handles, labels = [], []
     for ax in axes:
@@ -119,6 +127,7 @@ def plot(file, hostname, jobid):
 
     fig.legend(handles, labels, loc='lower center', bbox_to_anchor=(0.5, 0), frameon=False, ncol=2, title=f"bits for n={data['point_count'].iat[0]}, block_size={data['block_size'].iat[0]}, hostname={hostname}, jobid={jobid}")
     fig.set_size_inches(NUM_COLS * 2, NUM_ROWS * 2)
+    fig.subplots_adjust(bottom=0.04, top=0.99, right=0.99, wspace=0.3, hspace=0.5)
 
     # create directory if it does not exist
     os.makedirs("figures", exist_ok=True)
