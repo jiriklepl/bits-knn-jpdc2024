ee9936bfa6b7390275aab27c177c7bd353f36004
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2026-09-18 23:52:50 +0200

    fix air-topk and warpsort

--- include/bits/topk/multipass/air_topk.hpp
+++ include/bits/topk/multipass/air_topk.hpp
@@ -1,16 +1,30 @@
 #ifndef BITS_TOPK_MULTIPASS_AIR_TOPK_HPP_
 #define BITS_TOPK_MULTIPASS_AIR_TOPK_HPP_
 
+#include <memory>
 #include <string>
 
 #include "bits/cuda_knn.hpp"
 
+namespace raft
+{
+class resources;
+}
+
 class air_topk : public cuda_knn
 {
 public:
+    air_topk();
+    ~air_topk() override;
+
+    void initialize(const knn_args& args) override;
+
     std::string id() const override { return "air-topk"; }
 
     void selection() override;
+
+private:
+    std::unique_ptr<raft::resources> resources_;
 };
 
 #endif // BITS_TOPK_MULTIPASS_AIR_TOPK_HPP_
--- src/topk/singlepass/raft_warpsort.cu
+++ src/topk/singlepass/raft_warpsort.cu
@@ -1,7 +1,8 @@
 #include <cstddef>
 #include <cstdint>
 
-#include <raft/core/device_resources.hpp>
+#include <raft/core/resource/cuda_stream.hpp>
+#include <raft/core/resources.hpp>
 #include <raft/matrix/detail/select_warpsort.cuh>
 
 #include "bits/cuda_knn.hpp"
@@ -16,9 +17,12 @@ void raft_warpsort::selection()
     auto out_dist = out_dist_gpu();
     auto out_label = out_label_gpu();
 
-    raft::device_resources resources{};
+    raft::resources resources;
+    raft::resource::set_cuda_stream(resources,
+                                    rmm::cuda_stream_view{cuda_stream::make_default().get()});
 
-    raft::matrix::detail::select::warpsort::select_k(
+    raft::matrix::detail::select::warpsort::select_k<
+        float, std::int32_t, raft::matrix::detail::select::dense_layout<std::int32_t>>(
         resources, in_dist.data(), static_cast<std::int32_t*>(nullptr), in_dist.size(0),
         in_dist.size(1), k(), out_dist.data(), out_label.data(),
         true,                                 // select_min
