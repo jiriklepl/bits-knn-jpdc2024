cc8a728d23be40387e7acbdc3d4dfd98096c764d
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2026-09-19 13:58:06 +0200

    make the executor script more configurable

--- scripts/executor.sh
+++ scripts/executor.sh
@@ -4,22 +4,26 @@
 
 set -e
 
+NPROC=${NPROC:-"$(nproc)"}
+worker=${worker:-"$(hostname)"}
+builder=${builder:-"$(hostname)"}
+
 if [ -z "$build_dir" ]; then
     echo "Error: build_dir is not set."
     exit 1
 fi
 
 if [ "$3" == "build" ] || [ "$3" == "minimal-build" ] || [ "$3" == "all" ] || [ -z "$3" ]; then
-    run_single "$builder" cmake -B "$build_dir" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CUDA_ARCHITECTURES="$CUDA_ARCHITECTURES" -S .
+    run_single "$builder" cmake -B "$build_dir" -D CMAKE_BUILD_TYPE=Release -D CMAKE_CUDA_ARCHITECTURES="$CUDA_ARCHITECTURES" -S .
 
     if [ "$3" == "minimal-build" ]; then
-        run_single "$builder" cmake --build "$build_dir" --config Release --parallel 8 -t knn-minimal
-        run_single "$builder" cmake --build "$build_dir" --config Release --parallel 8 -t test
+        run_single "$builder" cmake --build "$build_dir" --config Release --parallel "$NPROC" -t knn-minimal
+        run_single "$builder" cmake --build "$build_dir" --config Release --parallel "$NPROC" -t test
         exit 0
     fi
 
-    run_single "$builder" cmake --build "$build_dir" --config Release --parallel 8 -t knn
-    run_single "$builder" cmake --build "$build_dir" --config Release --parallel 8 -t test
+    run_single "$builder" cmake --build "$build_dir" --config Release --parallel "$NPROC" -t knn
+    run_single "$builder" cmake --build "$build_dir" --config Release --parallel "$NPROC" -t test
 
     run_single "$worker" "$build_dir"/test
 
